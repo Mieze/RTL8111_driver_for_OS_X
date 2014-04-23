967ee68962e3d615cab39dcbeddc11dcc72498e2
Laura Müller <laura-mueller@uni-duesseldorf.de>
2014-04-23 03:56:54 +0200

    Version 1.2.0 - Now based on linux sources 8.037.00 which include support for additional RTL8111 family members. Uses a less agressive interrupt mitigate value for 10/100 mBit connections.

---
+++ RealtekRTL8111/RealtekRTL8111Linux-803700.cpp
@@ -0,0 +1,9 @@
+//
+//  RealtekRTL8111Linux-803700.cpp
+//  RealtekRTL8111
+//
+//  Created by Laura Müller on 22.01.14.
+//  Copyright (c) 2014 Laura Müller. All rights reserved.
+//
+
+#include "RealtekRTL8111Linux-803700.h"
---
+++ RealtekRTL8111/RealtekRTL8111Linux-803700.h
@@ -0,0 +1,14 @@
+//
+//  RealtekRTL8111Linux-803700.h
+//  RealtekRTL8111
+//
+//  Created by Laura Müller on 22.01.14.
+//  Copyright (c) 2014 Laura Müller. All rights reserved.
+//
+
+#ifndef RealtekRTL8111_RealtekRTL8111Linux_803700_h
+#define RealtekRTL8111_RealtekRTL8111Linux_803700_h
+
+
+
+#endif
